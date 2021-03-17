1cf182c420336653d8df6c4927785c78c7ad46e7
Thiago Hardt <thiago.hardts@gmail.com>
2021-03-17 13:24:27 +0000

    Finish deployment

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-//Server static assests in production
+//Server static assests in production.
 if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.user(express.static("client/build"));
